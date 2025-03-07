22a59dac8b129a02188356b1d7334465dbdb885c
FelipeKutschera <165940217+FelipeKutschera@users.noreply.github.com>
2025-03-06 22:24:24 -0300

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const HomePage = () => {
+
+  return (
+
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}> 
+
+    <text> PUC Minas </text>
+    
+    
+    </View>
+
+  );
+
+  }
+
+export default HomePage; 
